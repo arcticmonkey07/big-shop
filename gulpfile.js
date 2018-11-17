c3954717c61a77d69d71d05d96494abe92486e53
Peter <arcticmonkey07@gmail.com>
2018-11-17 05:04:46 +0300

    Сделал переключение табов

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,76 @@
 "use strict";
 
 var gulp = require("gulp");
+var del = require("del");
+var htmlmin = require("gulp-htmlmin");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var svgstore = require("gulp-svgstore");
+var minify = require("gulp-csso");
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 var posthtml = require("gulp-posthtml");
 var rename = require("gulp-rename");
 var server = require("browser-sync").create();
+var run = require("run-sequence");
 
-// Препроцессор, автопрефиксер, минификация CSS
-gulp.task("style", function() {
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("htmlmin", function () {
+  return gulp.src('source/*.html')
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest('build'));
+})
+
+gulp.task("style", function () {
   gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-// posthtml для вставки спрайта в разметку html-страниц, а также минификация кода html-страницы
-gulp.task("html", function() {
-  return gulp.src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest("source"));
+gulp.task("script", function () {
+  return gulp.src("source/js/*.js")
+    .pipe(uglify())
+    .pipe(concat("main.min.js"))
+    .pipe(gulp.dest("build/js"));
 });
 
-// Сервер Browser Sync
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  /**************************/
-  /* СЛЕДИМ ЗА ИЗМЕНЕНИЯМИ  */
-  /**************************/
-  // Следим за изменениями less-файлов
-  gulp.watch("source/sass/**/*.scss", ["style"]);
-    // Следим за изменениями html-страничек
-  gulp.watch("source/*.html").on("change", server.reload);
-  
+  gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("source/js/*.js", ["style"]);
+  gulp.watch("source/*.html", ["htmlmin"]).on("change", server.reload);
+});
+
+gulp.task("build", function () {
+  run("clean", "copy", "htmlmin", "style", "script");
 });
